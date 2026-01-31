487f85be0f533f984d330a2710939ca7c0b25d02
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-01-31 18:01:20 +0000

    feat: add home page to all responses

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 import http from "node:http";
+import fs from "node:fs";
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  console.log("response made");
-  res.end(
-    JSON.stringify({
-      data: "Hello World!",
-    })
-  );
+  console.log(req.url, req.method);
+
+  res.setHeader("Content-Type", "text/html");
+
+  fs.readFile("./pages/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
+
+  //   res.write("<p>hello world!</p>");
+  //   res.end();
+
+  //   res.writeHead(200, { "Content-Type": "application/json" });
+  //   console.log("response made");
+  //   res.end(
+  //     JSON.stringify({
+  //       data: "Hello World!",
+  //     })
+  //   );
 });
 
 server.listen(8000);
